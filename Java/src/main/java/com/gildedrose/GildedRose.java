353dd63a65417bd7818c0d91411fa533daa99ae8
Brandon Carlson <bcarlso@leantechniques.com>
2018-11-04 15:07:35 -0700

    Wasn't sure where to head with this so I simplified by using the 'for each' syntax on the loop. Got rid of all of that array indexing.

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -13,35 +13,35 @@ class GildedRose {
     }
 
     public void processEndOfDayUpdates() {
-        for (int i = 0; i < items.length; i++) {
-            if (AGED_BRIE.equals(items[i].name) || BACKSTAGE_PASSES.equals(items[i].name)) {
-                handleItemWithIncreasingQuality(items[i]);
+        for (Item item : items) {
+            if (AGED_BRIE.equals(item.name) || BACKSTAGE_PASSES.equals(item.name)) {
+                handleItemWithIncreasingQuality(item);
             } else {
-                if (hasQuality(items[i])) {
-                    if (!SULFURAS.equals(items[i].name)) {
-                        items[i].quality = items[i].quality - 1;
+                if (hasQuality(item)) {
+                    if (!SULFURAS.equals(item.name)) {
+                        item.quality = item.quality - 1;
                     }
                 }
             }
 
-            if (!SULFURAS.equals(items[i].name)) {
-                items[i].sellIn = items[i].sellIn - 1;
+            if (!SULFURAS.equals(item.name)) {
+                item.sellIn = item.sellIn - 1;
             }
 
-            if (items[i].sellIn < 0) {
-                if (!AGED_BRIE.equals(items[i].name)) {
-                    if (!BACKSTAGE_PASSES.equals(items[i].name)) {
-                        if (hasQuality(items[i])) {
-                            if (!SULFURAS.equals(items[i].name)) {
-                                items[i].quality = items[i].quality - 1;
+            if (item.sellIn < 0) {
+                if (!AGED_BRIE.equals(item.name)) {
+                    if (!BACKSTAGE_PASSES.equals(item.name)) {
+                        if (hasQuality(item)) {
+                            if (!SULFURAS.equals(item.name)) {
+                                item.quality = item.quality - 1;
                             }
                         }
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality;
+                        item.quality = item.quality - item.quality;
                     }
                 } else {
-                    if (items[i].quality < QUALITY_CEILING) {
-                        increaseQualityOf(items[i]);
+                    if (item.quality < QUALITY_CEILING) {
+                        increaseQualityOf(item);
                     }
                 }
             }
