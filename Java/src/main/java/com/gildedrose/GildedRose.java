0329128f1edf7f8398d45f6d5db055a5e22ec455
Brandon Carlson <bcarlso@leantechniques.com>
2018-11-04 13:34:34 -0700

    Extracted some constants and inverted the big if to get rid of 'not' logic.

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -4,6 +4,8 @@ class GildedRose {
     public static final String AGED_BRIE = "Aged Brie";
     public static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
     public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+    public static final int QUALITY_FLOOR = 0;
+    public static final int QUALITY_CEILING = 50;
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -12,29 +14,28 @@ class GildedRose {
 
     public void processEndOfDayUpdates() {
         for (int i = 0; i < items.length; i++) {
-            if (!AGED_BRIE.equals(items[i].name)
-                    && !BACKSTAGE_PASSES.equals(items[i].name)) {
-                if (items[i].quality > 0) {
-                    if (!SULFURAS.equals(items[i].name)) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
+            if (AGED_BRIE.equals(items[i].name) || BACKSTAGE_PASSES.equals(items[i].name)) {
+                if (items[i].quality < QUALITY_CEILING) {
                     items[i].quality = items[i].quality + 1;
 
                     if (BACKSTAGE_PASSES.equals(items[i].name)) {
                         if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
+                            if (items[i].quality < QUALITY_CEILING) {
                                 items[i].quality = items[i].quality + 1;
                             }
                         }
 
                         if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
+                            if (items[i].quality < QUALITY_CEILING) {
                                 items[i].quality = items[i].quality + 1;
                             }
                         }
+                    }
+                }
+            } else {
+                if (hasQuality(items[i])) {
+                    if (!SULFURAS.equals(items[i].name)) {
+                        items[i].quality = items[i].quality - 1;
                     }
                 }
             }
@@ -46,7 +47,7 @@ class GildedRose {
             if (items[i].sellIn < 0) {
                 if (!AGED_BRIE.equals(items[i].name)) {
                     if (!BACKSTAGE_PASSES.equals(items[i].name)) {
-                        if (items[i].quality > 0) {
+                        if (hasQuality(items[i])) {
                             if (!SULFURAS.equals(items[i].name)) {
                                 items[i].quality = items[i].quality - 1;
                             }
@@ -55,11 +56,15 @@ class GildedRose {
                         items[i].quality = items[i].quality - items[i].quality;
                     }
                 } else {
-                    if (items[i].quality < 50) {
+                    if (items[i].quality < QUALITY_CEILING) {
                         items[i].quality = items[i].quality + 1;
                     }
                 }
             }
         }
+    }
+
+    private boolean hasQuality(Item item) {
+        return item.quality > QUALITY_FLOOR;
     }
 }
