91c15d478c0da32a3465b66ab8606a080f2ea13b
Brandon Carlson <bcarlso@leantechniques.com>
2018-11-04 22:02:07 -0500

    Removing need for GildedRose to use Item directly.

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -2,30 +2,10 @@ package com.gildedrose;
 
 class GildedRose {
 
-    Item[] items;
     private final EndOfDayItemProcessor[] itemProcessors;
-
-    public GildedRose(Item[] items) {
-        this.items = items;
-        itemProcessors = new EndOfDayItemProcessor[items.length];
-        for (int i = 0; i < items.length; i++) {
-            if (Sulfuras.SULFURAS.equals(items[i].name))
-                itemProcessors[i] = (new Sulfuras(items[i]));
-            else if (AgedBrie.AGED_BRIE.equals(items[i].name))
-                itemProcessors[i] = (new AgedBrie(items[i]));
-            else if (BackstagePasses.BACKSTAGE_PASSES.equals(items[i].name))
-                itemProcessors[i] = (new BackstagePasses(items[i]));
-            else
-                itemProcessors[i] = (new StandardItem(items[i]));
-        }
-    }
 
     public GildedRose(EndOfDayItemProcessor[] items) {
         itemProcessors = items;
-        this.items = new Item[itemProcessors.length];
-        for(int i = 0; i < items.length; i++) {
-            this.items[i] = itemProcessors[i].item;
-        }
     }
 
     public void processEndOfDayUpdates() {
@@ -34,15 +14,15 @@ class GildedRose {
         }
     }
 
-    public Item item(int i) {
-        return items[i];
+    public EndOfDayItemProcessor item(int i) {
+        return itemProcessors[i];
     }
 
     int getSellInForItem(int i) {
-        return item(i).sellIn;
+        return item(i).sellIn();
     }
 
     int getQualityForItem(int i) {
-        return item(i).quality;
+        return item(i).quality();
     }
 }
